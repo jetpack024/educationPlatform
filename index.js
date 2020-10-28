474fa274ba6c161ab509b5a5b09fe1367273db8c
Aleksei-web <maksimushkin92@gmail.com>
2020-10-28 17:44:34 +0300

    startProject

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const path = require('path');
+const Handlebars = require('handlebars');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
+const exphbs = require('express-handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const cardRoutes = require('./routes/card');
+const ordersRoutes = require('./routes/orders');
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
+const authRoutes = require('./routes/auth');
+const User = require('./models/user');
+const varMiddelware = require('./middelware/variables');
+const userMiddeleware = require('./middelware/user');
+
+const MONGODB_URL = 'mongodb://localhost:27017/shop';
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+});
+
+const store = new MongoStore({
+  collection: 'sessions',
+  url: MONGODB_URL,
+});
+
+app.engine('hbs', hbs.engine); // регестрируем движок
+app.set('view engine', 'hbs'); // начинаем использовать
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  store,
+}));
+app.use(varMiddelware);
+app.use(userMiddeleware);
+
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 8080;
+
+async function start() {
+  try {
+    await mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is runing on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
---
+++ public/app.js
@@ -0,0 +1,58 @@
+const toCurrenscy = price => {
+  return new Intl.NumberFormat('ru-Ru', {
+    currency: 'rub',
+    style: 'currency'
+  }).format(price)
+}
+
+const toDate = date => {
+  return new Intl.DateTimeFormat('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }).format(new Date(date))
+}
+
+document.querySelectorAll('.price').forEach(node => {
+  node.textContent = toCurrenscy(node.textContent)
+})
+
+document.querySelectorAll('.date').forEach(node => {
+  node.textContent = toDate(node.textContent)
+})
+
+const $card = document.querySelector('#card')
+if ($card) {
+  $card.addEventListener('click', (event) => {
+    if (event.target.classList.contains('js-remove')) {
+      const id = event.target.dataset.id;
+
+      fetch('/card/remove/' + id, {
+        method: 'delete',
+      }).then(res => res.json())
+        .then(card => {
+          if (card.courses.length) {
+            const html = card.courses.map((el) => `
+              <tr>
+              <td>${el.title}</td>
+              <td>${el.count}</td>
+              <td>
+                <button class="btn btn-small js-remove" data-id="${el.id}">Удалить</button>
+              </td>
+            </tr>
+              `).join('')
+            $card.querySelector('tbody').innerHTML = html
+            $card.querySelector('.price').textContent = toCurrenscy(card.price)
+          } else {
+            $card.innerHTML = '<p>Корзина пуста</p>'
+          }
+        })
+    }
+  })
+}
+
+M.Tabs.init(document.querySelectorAll('.tabs')); 
+
