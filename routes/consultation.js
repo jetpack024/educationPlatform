44f3e3221d81c3ef4686fbcd7ade996f122c43ed
Kristina Bagrova <kristinabagrovaa@gmail.com>
2020-10-29 19:56:25 +0300

    Consultation requests page for admin added

--- routes/consultation.js
+++ routes/consultation.js
@@ -5,15 +5,17 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   res.render('consultation');
-});
-
-router.get('/confirm', async (req, res) => {
-  const { user } = req.session;
-  const mongoUser = await User.findOne({ _id: user._id });
-  mongoUser.consultationRequest = true;
-  await mongoUser.save();
-  console.log(mongoUser);
-  res.render('consultation-confirmed');
-});
+})
+  .get('/confirm', async (req, res) => {
+    const { user } = req.session;
+    const mongoUser = await User.findOne({ _id: user._id });
+    mongoUser.consultationRequest = true;
+    await mongoUser.save();
+    res.render('consultation-confirmed');
+  })
+  .get('/requests', async (req, res) => {
+    const users = await User.find({ consultationRequest: true });
+    res.render('consultation-requests', { users });
+  });
 
 module.exports = router;
