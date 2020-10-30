b0a05a23c97193d2f1ec4dfa72e0dd76f334e00b
Kristina Bagrova <kristinabagrovaa@gmail.com>
2020-10-30 12:25:51 +0300

    User Page added

--- routes/user.js
+++ routes/user.js
@@ -6,26 +6,45 @@ const router = Router();
 
 router
   .get('/', auth, async (req, res) => {
-    res.render('user', {
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email,
-      phoneNumber: req.user.phoneNumber,
-    });
+    const { user } = req;
+    console.log(user);
+    res.render('user', { user });
   })
   .get('/edit', auth, async (req, res) => {
     const { user } = req.session;
     res.render('user-edit', { user });
+  })
+  .post('/edit', auth, async (req, res) => {
+    // try {
+    const { user } = req.session;
+    const mongoUser = await User.findOne({ _id: user._id });
+    const {
+      name,
+      lastName,
+      middleName,
+      address,
+      childFirstName,
+      childMiddleName,
+      childLastName,
+      DateOfBirth,
+    } = req.body;
+    mongoUser.name = name;
+    mongoUser.lastName = lastName;
+    mongoUser.middleName = middleName;
+    mongoUser.address = address;
+    const child = {
+      childFirstName, childMiddleName, childLastName, DateOfBirth,
+    };
+    mongoUser.children = child;
+    mongoUser.isSuperUser = true;
+    req.session.isSuperUser = true;
+    console.log('mongoUser', mongoUser);
+    await mongoUser.save();
+
+    res.redirect('/user');
+    // } catch (e) {
+    //   console.log(e);
+    // }
   });
-
-// router.post('/', auth, async (req, res) => {
-//   try {
-//     const {} = req.body;
-
-//     res.redirect('/orders');
-//   } catch (e) {
-//     console.log(e);
-//   }
-// });
 
 module.exports = router;
